5b3fecd1b32755d11d4a15b039ce186eaa928c8f
gnoronha42 <gabrielximenesnoronha@gmail.com>
2024-08-15 11:38:27 -0300

    feat:add new background

---
+++ src/assets/index.ts
@@ -0,0 +1,16 @@
+export {default as IMG_6066} from './inhotim/IMG_6066.png';
+export {default as IMG_6114} from './inhotim/IMG_6114.png';
+export {default as IMG_6115} from './inhotim/IMG_6115.jpg';
+export {default as IMG_6133} from './inhotim/IMG_6133.jpg';
+export {default as IMG_6157} from './inhotim/IMG_6157.jpg';
+export {default as IMG_6192} from './inhotim/IMG_6192.jpg';
+export {default as IMG_6193} from './inhotim/IMG_6193.jpg';
+export {default as IMG_6219} from './inhotim/IMG_6219.jpg';
+export {default as IMG_6334} from './inhotim/IMG_6334.jpg';
+export {default as IMG_6435} from './inhotim/IMG_6435.jpg';
+export {default as IMG_6436} from './inhotim/IMG_6436.jpg';
+export {default as IMG_6457} from './inhotim/IMG_6457.jpg';
+export {default as IMG_6624} from './inhotim/IMG_6624.jpg';
+export {default as IMG_6646} from './inhotim/IMG_6646.jpg';
+export {default as IMG_6649} from './inhotim/IMG_6649.jpg';
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,65 @@
 import { Menu } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
-
+import {
+    IMG_6066,
+    IMG_6114,
+    IMG_6115,
+    IMG_6133,
+    IMG_6157,
+    IMG_6192,
+    IMG_6193,
+    IMG_6219,
+    IMG_6334,
+    IMG_6435,
+    IMG_6436,
+    IMG_6457,
+    IMG_6624,
+    IMG_6646,
+    IMG_6649,
+} from '../../assets/';
 
 const Home = () => {
+    const images = [
+        IMG_6066,
+        IMG_6114,
+        IMG_6115,
+        IMG_6133,
+        IMG_6157,
+        IMG_6192,
+        IMG_6193,
+        IMG_6219,
+        IMG_6334,
+        IMG_6435,
+        IMG_6436,
+        IMG_6457,
+        IMG_6624,
+        IMG_6646,
+        IMG_6649,
+    ];
+    const [backgroundImage, setBackgroundImage] = useState(images[0]);
+
     const items = [
         { key: '1', label: 'Why we do' },
         { key: '2', label: 'The things we do' },
         { key: '3', label: 'Says a lot' },
         { key: '4', label: 'About us' },
-      ];
+    ];
+
+    useEffect(() => {
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+        setBackgroundImage(randomImage);
+       
+    }, []);
+
+    const handleMenuClick = () => {
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+        setBackgroundImage(randomImage);
+    };
+
   
       return(
-   <div className="home" >
+   <div className="home" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Menu
           theme="dark"
           mode="horizontal"
@@ -20,6 +67,7 @@ const Home = () => {
           defaultSelectedKeys={['2']}
           items={items}
           style={{ flex: 1, minWidth: 0 }}
+          onClick={handleMenuClick}
         />
 
 <h1 className="title">HOW TO SAMBA?</h1> {/* Título adicionado */}
