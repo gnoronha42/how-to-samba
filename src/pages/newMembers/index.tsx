de47c4c04f7ed762cfe60f0fb02072a4ebc0be17
gnoronha42 <gabrielximenesnoronha@gmail.com>
2024-08-27 06:02:26 -0300

    feat:change time

--- src/pages/newMembers/index.tsx
+++ src/pages/newMembers/index.tsx
@@ -18,17 +18,17 @@ const NewMembersForm: React.FC = () => {
       ...values,
       date: currentDate,
     };
-  
-    // Check for duplicate keys
+
+    
     const keys = Object.keys(dataToSend);
     const uniqueKeys = new Set(keys);
     if (keys.length !== uniqueKeys.size) {
       console.error('Duplicate keys found in dataToSend:', dataToSend);
       return;
     }
-  
+
     console.log('Data to send:', dataToSend);
-  
+
     try {
       const response = await fetch('https://sheet.best/api/sheets/bb3d98f0-83b6-4bc3-ba5b-72b9d60f04fc', {
         method: 'POST',
@@ -37,19 +37,32 @@ const NewMembersForm: React.FC = () => {
         },
         body: JSON.stringify(dataToSend),
       });
-  
+
       if (response.ok) {
         console.log('Dados enviados com sucesso:', dataToSend);
+        
+        const emailResponse = await fetch('https://formspree.io/f/mnnawpyb', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email: values.email, message: 'Thank you for registering. We are excited to have you on board!' }),
+        });
+
+        if (emailResponse.ok) {
+          console.log('Email enviado com sucesso');
+        } else {
+          console.error('Erro ao enviar email:', emailResponse.statusText);
+        }
       } else {
         console.error('Erro ao enviar dados:', response.statusText);
       }
     } catch (error) {
       console.error('Erro ao enviar dados:', error);
     }
-  
+
     form.resetFields();
   };
-
   return (
     <div className="page-container" style={{backgroundImage: `url(${backgroundImage})`}}>
     
@@ -67,7 +80,7 @@ const NewMembersForm: React.FC = () => {
             layout="vertical"
           >
             <Form.Item
-              name="email"
+              name="Email"
               rules={[
                 { required: true, message: t('please_enter_email') },
                 { type: 'email', message: t('please_enter_valid_email') }
